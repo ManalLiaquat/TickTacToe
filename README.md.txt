c2f92468672a0e8005b56e6f36e72fecb3224d6a
ManalLiaquat <m.manalliaquat@gmail.com>
2018-12-01 21:47:09 +0500

    README.md

---
+++ README.md.txt
@@ -0,0 +1,12 @@
+# Tic Tac Toe - The Game
+
+
+
+
+Play this simple game and expertize your skills.
+
+![screenshot](https://i.imgur.com/Is01NZe.png)
+
+
+
+### Play Game: [Tic Tac Toe](https://codepen.io/ManalLiaquat/full/qYZPwq)
--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ var filesToCache = [
 
 self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Install');
+    skipWaiting();
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
             console.log('[ServiceWorker] Caching app shell');
